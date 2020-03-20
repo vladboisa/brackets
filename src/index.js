a24d8da646ec91af9f8f4389c43bf75f428d210d
VladBoisa <60569670+vladboisa@users.noreply.github.com>
2020-03-20 16:29:41 +0300

    feat: add bad solut

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    for (let i = 0; i < str.length; i++) {
+        bracketsConfig.forEach(el => {
+            let pairBracket = el.join('');
+            let indexstr = str.indexOf(el);
+            if (indexstr !== -1) {
+                str = str.replace(pairBracket, '');
+            }
+        });
+    }
+    return !str;
 }
